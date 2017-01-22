2973b7a3a7e27696fdd7d64f029ba5a0780a1373
Naziba <Naziba@nazibas-mbp.wireless.concordia.ca>
2017-01-21 23:11:38 -0500

    Add login

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-
+angular.module('starter', ['ionic', 'ngMockE2E'])
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
@@ -22,4 +22,56 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function ($stateProvider, $urlRouterProvider, USER_ROLES) {
+  $stateProvider
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
+  .state('main', {
+    url: '/',
+    abstract: true,
+    templateUrl: 'templates/main.html'
+  })
+  .state('main.dash', {
+    url: 'main/dash',
+    views: {
+        'dash-tab': {
+          templateUrl: 'templates/dashboard.html',
+          controller: 'DashCtrl'
+        }
+    }
+  })
+  .state('main.public', {
+    url: 'main/public',
+    views: {
+        'public-tab': {
+          templateUrl: 'templates/public.html'
+        }
+    }
+  })
+  .state('main.admin', {
+    url: 'main/admin',
+    views: {
+        'admin-tab': {
+          templateUrl: 'templates/admin.html'
+        }
+    },
+    data: {
+      authorizedRoles: [USER_ROLES.admin]
+    }
+  });
+
+  // Thanks to Ben Noblet!
+  $urlRouterProvider.otherwise(function ($injector, $location) {
+    var $state = $injector.get("$state");
+    $state.go("main.dash");
+  });
+})
+
+.run(function($httpBackend){
+  $httpBackend.whenGET(/templates\/\w+.*/).passThrough();
 })
---
+++ www/js/controller.js
@@ -0,0 +1,5 @@
+angular.module('starter')
+
+.controller('AppCtrl', function() {})
+.controller('LoginCtrl', function() {})
+.controller('DashCtrl', function() {});
