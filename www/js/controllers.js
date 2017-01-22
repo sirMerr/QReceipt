07530b1cc6cdd294df19dd3f73c3090d0c24553d
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2017-01-22 03:40:58 -0500

    :wrench: Fix logout's mysterious disappearance

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,71 +1,95 @@
 angular.module('starter')
 
-.controller('AppCtrl', function($scope, $state, $ionicPopup, AuthService, AUTH_EVENTS) {
-  $scope.username = AuthService.username();
+	.controller('AppCtrl', function($scope, $state, $ionicPopup, AuthService, AUTH_EVENTS) {
+		$scope.username = AuthService.username();
 
-  $scope.$on(AUTH_EVENTS.notAuthorized, function(event) {
-    var alertPopup = $ionicPopup.alert({
-      title: 'Unauthorized!',
-      template: 'You are not allowed to access this resource.'
-    });
-  });
+		$scope.$on(AUTH_EVENTS.notAuthorized, function(event) {
+			var alertPopup = $ionicPopup.alert({
+				title: 'Unauthorized!',
+				template: 'You are not allowed to access this resource.'
+			});
+		});
 
-  $scope.$on(AUTH_EVENTS.notAuthenticated, function(event) {
-    AuthService.logout();
-    $state.go('login');
-    var alertPopup = $ionicPopup.alert({
-      title: 'Session Lost!',
-      template: 'Sorry, You have to login again.'
-    });
-  });
+		$scope.$on(AUTH_EVENTS.notAuthenticated, function(event) {
+			AuthService.logout();
+			$state.go('login');
+			var alertPopup = $ionicPopup.alert({
+				title: 'Session Lost!',
+				template: 'Sorry, You have to login again.'
+			});
+		});
 
-  $scope.setCurrentUsername = function(name) {
-    $scope.username = name;
-  };
-})
-.controller('LoginCtrl', function($scope, $state, $ionicPopup, AuthService) {
-  $scope.data = {};
+		$scope.setCurrentUsername = function(name) {
+			$scope.username = name;
+		};
+	})
+	.controller('LoginCtrl', function($scope, $state, $ionicPopup, AuthService) {
+		$scope.data = {};
 
-  $scope.login = function(data) {
-    AuthService.login(data.username, data.password).then(function(authenticated) {
-      $state.go('main.dash', {}, {reload: true});
-      $scope.setCurrentUsername(data.username);
-    }, function(err) {
-      var alertPopup = $ionicPopup.alert({
-        title: 'Login failed!',
-        template: 'Please check your credentials!'
-      });
-    });
-  };
-})
-.controller('DashCtrl', function($scope, $state, $http, $ionicPopup, AuthService) {
-  $scope.logout = function() {
-    AuthService.logout();
-    $state.go('login');
-  };
+		$scope.login = function(data) {
+			AuthService.login(data.username, data.password).then(function(authenticated) {
+				$state.go('main.dash', {}, {
+					reload: true
+				});
+				$scope.setCurrentUsername(data.username);
+			}, function(err) {
+				var alertPopup = $ionicPopup.alert({
+					title: 'Login failed!',
+					template: 'Please check your credentials!'
+				});
+			});
+		};
+	})
+	.controller('DashCtrl', function($scope, $state, $http, $ionicPopup, AuthService) {
+		$scope.groups = [];
+		for (var i = 0; i < 10; i++) {
+			$scope.groups[i] = {
+				name: i,
+				items: []
+			};
+			for (var j = 0; j < 3; j++) {
+				$scope.groups[i].items.push(i + '-' + j);
+			}
+		}
+		$scope.toggleGroup = function(group) {
+			if ($scope.isGroupShown(group)) {
+				$scope.shownGroup = null;
+			} else {
+				$scope.shownGroup = group;
+			}
+		};
+		$scope.isGroupShown = function(group) {
+			return $scope.shownGroup === group;
+		};
+		$scope.logout = function() {
+			AuthService.logout();
+			$state.go('login');
+		};
 
-  $scope.performValidRequest = function() {
-    $http.get('http://localhost:8100/valid').then(
-      function(result) {
-        $scope.response = result;
-      });
-  };
+		$scope.performValidRequest = function() {
+			$http.get('http://localhost:8100/valid').then(
+				function(result) {
+					$scope.response = result;
+				});
+		};
 
-  $scope.performUnauthorizedRequest = function() {
-    $http.get('http://localhost:8100/notauthorized').then(
-      function(result) {
-        // No result here..
-      }, function(err) {
-        $scope.response = err;
-      });
-  };
+		$scope.performUnauthorizedRequest = function() {
+			$http.get('http://localhost:8100/notauthorized').then(
+				function(result) {
+					// No result here..
+				},
+				function(err) {
+					$scope.response = err;
+				});
+		};
 
-  $scope.performInvalidRequest = function() {
-    $http.get('http://localhost:8100/notauthenticated').then(
-      function(result) {
-        // No result here..
-      }, function(err) {
-        $scope.response = err;
-      });
-  };
-});
+		$scope.performInvalidRequest = function() {
+			$http.get('http://localhost:8100/notauthenticated').then(
+				function(result) {
+					// No result here..
+				},
+				function(err) {
+					$scope.response = err;
+				});
+		};
+	});
